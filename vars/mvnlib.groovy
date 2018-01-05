f7ee2cadb1ef4f50d2104c068878ba5eade993d1
Sanjana.T <sanjana.t@Sanjanas-MacBook-Pro.local>
2018-01-05 14:58:02 +0530

    maven build library

---
+++ vars/mvnlib.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call(String test = 'false') {
+	  echo "test=${test}"
+	  if (test.equalsIgnoreCase('false')) {
+	    mavenJob('mavenjob') {
+	      goals('clean install -DskipTests')
+	    }
+	  } else{
+	    mavenJob('mavenjob') {
+	      goals('clean install')
+	    }
+	  }
+	}
+
