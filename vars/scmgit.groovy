9c69569d945de510b36c5d5d2dffdba0b1fb5848
MSanjanatelkar <34422443+MSanjanatelkar@users.noreply.github.com>
2018-01-05 15:20:21 +0530

    Update scmgit.groovy

--- vars/scmgit.groovy
+++ vars/scmgit.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def checkOutFrom(giturl) {
-  node{
-    stage (connecting to scm)
+def call(giturl) {
+  node(){
+    stage ("connecting to scm")
     {  
       checkout scm
     }
