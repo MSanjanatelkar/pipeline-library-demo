3f1cbc1e1069a53e6cc01d9fb43e1c7ba5646248
MSanjanatelkar <34422443+MSanjanatelkar@users.noreply.github.com>
2018-01-05 16:30:38 +0530

    Update mvnlib.groovy

--- vars/mvnlib.groovy
+++ vars/mvnlib.groovy
@@ -1,15 +1,23 @@
-#!/usr/bin/env groovy
+!/usr/bin/env groovy
 
 def call(String test = 'false') {
-	  echo "test=${test}"
-	  if (test.equalsIgnoreCase('false')) {
-	    mavenJob('mavenjob') {
-	      goals('clean install -DskipTests')
-	    }
-	  } else{
-	    mavenJob('mavenjob') {
-	      goals('clean install')
-	    }
-	  }
-	}
+  echo "received test=${test}"
 
+  if (test.equalsIgnoreCase('false')) {
+    echo "running for test false"
+    stages {
+      stage ('install') {
+        steps {
+          withMaven(maven : 'maven_3_5_0') {
+          sh 'mvn clean install -Dskiptest'
+          }
+        }
+      }
+    }
+  } else{
+    echo "running for test true"
+    maven {
+      sh 'mvn clean install'
+    }
+  }
+}
